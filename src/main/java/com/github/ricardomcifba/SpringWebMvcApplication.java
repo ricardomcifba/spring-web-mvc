ced5001abbd6e1e56e33b653d6f77236cb093791
Ricardo Carvalho <rickmc123@gmail.com>
2021-09-17 15:32:42 -0300

    Iniciando projeto

---
+++ src/main/java/com/github/ricardomcifba/SpringWebMvcApplication.java
@@ -0,0 +1,14 @@
+package com.github.ricardomcifba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcApplication.class, args);
+		//System.out.println("Ricardo");
+	}
+
+}
---
+++ src/test/java/com/github/ricardomcifba/SpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ricardomcifba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
